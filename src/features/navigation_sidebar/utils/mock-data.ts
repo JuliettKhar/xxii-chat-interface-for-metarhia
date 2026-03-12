b7672e7ad020ba46fe872e5498bd6a295e54d450
julia kharlamova <juliettkhar@gmail.com>
2026-03-12 13:56:57 +0300

    add mockdata

---
+++ src/features/navigation_sidebar/utils/mock-data.ts
@@ -0,0 +1,71 @@
+export const CONTACTS = [
+  {
+    name: 'name 1',
+    icon: 'person',
+    lastActivity: '12:41',
+  },
+  {
+    name: 'name 2',
+    icon: 'person',
+    lastActivity: '11:23',
+  },
+  {
+    name: 'name 3',
+    icon: 'person',
+    lastActivity: 'yesterday',
+  },
+];
+
+export const FEEDS = [
+  {
+    name: '#announcement',
+    icon: 'campaign',
+    lastActivity: '12:32',
+  },
+  {
+    name: '#dev',
+    icon: 'code',
+    lastActivity: '10:12',
+  },
+  {
+    name: '#design',
+    icon: 'design_services',
+    lastActivity: 'yesterday',
+  },
+  {
+    name: '#random',
+    icon: 'shuffle',
+    lastActivity: 'yesterday',
+  },
+];
+
+export const CHATS = [
+  {
+    name: '!engineering',
+    icon: 'folder_code',
+    lastActivity: '12:01',
+  },
+  {
+    name: '!product',
+    icon: 'contacts_product',
+    lastActivity: '11:04',
+  },
+  {
+    name: '!general',
+    icon: 'settings_heart',
+    lastActivity: 'yesterday',
+  },
+];
+
+export const FOLDERS = [
+  {
+    name: 'Projects',
+    icon: 'folder',
+    files: ['project-alpha', 'project-beta'],
+  },
+  {
+    name: 'Archive',
+    icon: 'folder',
+    files: ['old-chats'],
+  },
+];
